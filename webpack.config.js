0338c1a3f84a4a78e368edb10f3afe3ebc5d0e49
Abdul Muqeet <39890909+abdulmuqeet1@users.noreply.github.com>
2021-09-10 10:23:57 +0500

    asset modules update
    removal of deprecated file loader in webpack v5 and addition of asset modules

--- webpack.config.js
+++ webpack.config.js
@@ -46,11 +46,7 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: "file-loader",
-        options: {
-          outputPath: "assets",
-          publicPath: "./assets",
-        },
+        type: "asset/resource",
       },
     ],
   },
