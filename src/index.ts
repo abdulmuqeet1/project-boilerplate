e391d79120b8e4c942dbf7fded2d4af172f819d7
Abdul Muqeet <39890909+abdulmuqeet1@users.noreply.github.com>
2022-04-09 00:18:20 +0500

    threejs template added

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import './styles/main.scss';
+
+export const addInt = (a: number, b: number): number => {
+    return a + b;
+};
+
+console.log('answer: ', addInt(1, 2));
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,62 @@
-const path = require("path");
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // generates a html file in output dir with bundled js file loaded in it
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // unlike css-loader, it create css files
-const loader = require("sass-loader");
-// use style loader for injecting the css file into (output)js file
-const TerserPlugin = require("terser-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // unlike css-loader, it create css files
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  devtool: "source-map",
-  target: "web",
+    target: 'web',
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, 'src/index.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].js',
+        clean: true,
+    },
 
-  // entry point
-  entry: "./src",
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+    },
 
-  // output point
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: 'swc-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            },
+            {
+                test: /.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 
-  resolve: {
-    extensions: [".js", ".ts", ".tsx", ".json"],
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts|tsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader", // use ts-loader with babel loader if .babelrc file doesnt contains @babel/preset-typescript
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader",
-      },
-      {
-        test: /.scss$/,
-        // publicPath: "./",
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-        // use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        type: "asset/resource",
-      },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            // inject: true,
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css',
+            chunkFilename: '[id].[hash].css',
+        }),
+        new ForkTsCheckerWebpackPlugin(),
     ],
-  },
-
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
-  },
-
-  plugins: [
-    new ForkTsCheckerWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/template/index.html",
-      filename: "./index.html",
-    }),
-    new MiniCssExtractPlugin(),
-  ],
 };
